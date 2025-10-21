a5fceff72bf7ad376efc50f1ad8b0e353bed7492
speedlin1006 <speedlin10060106@gmail.com>
2025-10-21 17:02:40 +0800

    新增抓位置及IP

--- server/models/loginIpModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose"
-
-const loginIpSchema = new mongoose.Schema({
-  account: String,
-  ip: String,
-  loginTime: { type: Date, default: Date.now }
-})
-
-export default mongoose.model("LoginIp", loginIpSchema)
